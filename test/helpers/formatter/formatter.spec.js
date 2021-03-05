dba0cb95073105bf6a5a3fdd79887281961d4c09
Johnny Vanhentenryk <johnny.vanhentenryk@bnpparibasfortis.com>
2021-03-05 09:35:50 +0100

    Replace node-sass by sass

--- test/helpers/formatter/formatter.spec.js
+++ test/helpers/formatter/formatter.spec.js
@@ -5,6 +5,5 @@ const formatter = require('../../../src/helpers/formatter/formatter')
 describe("Formatter testing", () => {
     it("Should format IBAN correctly", () => {
         assert.strictEqual(formatter.formatIBAN('BE19001288305634'), 'BE000000000')  
-        
     });
 });
