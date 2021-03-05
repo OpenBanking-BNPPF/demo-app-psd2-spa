b417be4033b3faec50876f147d6b58d318408fc5
Johnny Vanhentenryk <johnny.vanhentenryk@bnpparibasfortis.com>
2021-03-05 09:07:42 +0100

    add travis pipeline

---
+++ test/helpers/formatter/formatter.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const formatter = require('../../../src/helpers/formatter/formatter')
+
+
+describe("Formatter testing", () => {
+    it("Should format IBAN correctly", () => {
+        assert.strictEqual(formatter.formatIBAN('BE19001288305634'), 'BE000000000')  
+        
+    });
+});
